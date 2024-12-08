fe82ac5a1e95a3404cf7413885ee8e0e35ddcef1
Le Vi <trieuvi27.le@gmail.com>
2024-12-08 11:09:58 +0700

    change frontend to test pipeline

--- app-code/frontend/notes-frontend/src/components/Notes.js
+++ app-code/frontend/notes-frontend/src/components/Notes.js
@@ -38,7 +38,7 @@ const Notes = () => {
                 <Box display="flex" justifyContent="space-between">
                     <Box flex={1} mr={4}>
                         <Typography variant="h4" gutterBottom sx={{ textAlign: 'center', marginBottom: 4 }}>
-                            Add a New Note
+                            Add a New Note NT548
                         </Typography>
                         <Box
                             mb={4}
